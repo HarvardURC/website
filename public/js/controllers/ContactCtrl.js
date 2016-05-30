fcd96c981d73a7d9fc070391441985c9524e3842
Timothy tamm <tamm@college.harvard.edu>
2016-05-30 20:14:26 +0300

    landing page

---
+++ public/js/controllers/ContactCtrl.js
@@ -0,0 +1,6 @@
+
+// public/js/controllers/ContactCtrl.js
+angular.module('ContactCtrl', [])
+.controller('ContactController', ['$scope', function($scope) {
+
+}]);
---
+++ public/js/controllers/LandingCtrl.js
@@ -0,0 +1,6 @@
+
+// public/js/controllers/LandingCtrl.js
+angular.module('LandingCtrl', [])
+.controller('LandingController', ['$scope', function($scope) {
+
+}]);
