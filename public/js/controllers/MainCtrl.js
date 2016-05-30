8fcab9a36a7659ad9bdbf13023fc65b56cda4148
Timothy tamm <tamm@college.harvard.edu>
2016-05-31 00:35:51 +0300

    mobile optimization

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,27 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', [])
-.controller('MainController', ['$scope', function($scope) {
+.controller('MainController', ['$scope', '$window', function($scope, $window) {
+    if ($window.innerWidth < 768){
+        $scope.includeMobileTemplate = true;
+    } else {
+        $scope.includeMobileTemplate = false;
+    }
+
+
+    $(window).on("resize.doResize", function (){
+
+      $scope.$apply(function(){
+          if (window.innerWidth < 768){
+              $scope.includeMobileTemplate = true;
+          } else {
+              $scope.includeMobileTemplate = false;
+          }
+      });
+      console.log($scope.includeMobileTemplate);
+  });
+
+  $scope.$on("$destroy",function (){
+      $(window).off("resize.doResize"); //remove the handler added earlier
+  });
 
 }]);
