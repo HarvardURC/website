bb3be886225203aee5e20aa80eefa98b7510c39b
Timothy tamm <tamm@college.harvard.edu>
2016-06-05 01:50:09 +0100

    mw page

--- public/js/controllers/ContactCtrl.js
+++ public/js/controllers/ContactCtrl.js
@@ -2,8 +2,19 @@
 angular.module('ContactCtrl', [])
 .controller('ContactController', function($http, $scope) {
 
+    $scope.isSuccess = false;
+    $scope.button_text = "Sign up";
+
     $scope.subscribe = function (application) {
-        return $http.post('/api/subscribe', application);
+        if (application.first_name &&
+            application.last_name &&
+            application.email) {
+                $scope.isSuccess = true;
+                $scope.button_text = "Subscribed";
+                return $http.post('/api/subscribe', application);
+            }
     };
+
+
 
 });
--- public/js/controllers/SignupCtrl.js
+++ public/js/controllers/SignupCtrl.js
@@ -2,8 +2,23 @@
 angular.module('SignupCtrl', [])
 .controller('SignupController', function($http, $scope) {
 
+    $scope.isSuccess = false;
+    $scope.button_text = "Apply";
     $scope.apply = function (application) {
-        return $http.post('/api/signup', application);
+        if (application.first_name &&
+            application.last_name &&
+            application.email &&
+            application.concentration &&
+            application.first_choice &&
+            application.second_choice &&
+            application.third_choice &&
+            application.class &&
+            application.reason &&
+            application.background) {
+                $scope.isSuccess = true;
+                $scope.button_text = "Submitted";
+                return $http.post('/api/signup', application);
+            }
     };
 
 });
